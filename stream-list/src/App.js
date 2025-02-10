3b161692101fda337336f9b4fee65614a5a709af
Latesha Carter <159862436+latesha-carter@users.noreply.github.com>
2025-02-09 17:11:54 -0700

    changes to make errors stop

--- stream-list/src/App.js
+++ stream-list/src/App.js
@@ -1,5 +1,6 @@
+// filepath: /Users/lateshabrewer-carter/stream-list/stream-list/src/App.js
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import StreamList from './components/StreamList';
 import Movies from './components/Movies';
@@ -8,27 +9,29 @@ import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">StreamList</h1>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/movies">Movies</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Switch>
-          <Route exact path="/" component={StreamList} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">StreamList</h1>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/movies">Movies</Link></li>
+              <li><Link to="/cart">Cart</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<StreamList />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
